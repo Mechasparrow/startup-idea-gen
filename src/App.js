ec6e1ffdb0ce96b6b89cf0bffa7abab8741549d4
Michael Navazhylau <mikipux7@gmail.com>
2017-03-12 17:10:43 -0500

    Made some modifications

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = "app container-fluid">
+        <h1 className = "app-header"><b>Startup matrix generator</b></h1>
+      
       </div>
     );
   }
